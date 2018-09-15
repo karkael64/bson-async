962abb8e712f6d9f3e4445115bf0432f1a3056dc
Victor COURTIN <victor.courtin-prestataire@docapost.fr>
2018-09-15 02:35:47 +0200

    init

---
+++ bson.js
@@ -0,0 +1,197 @@
+const File = require('./file');
+
+function is_function(el) {
+    return (typeof el === 'function');
+}
+
+function is_number(el) {
+    return (typeof el === 'number');
+}
+
+function is_object(el) {
+    return (typeof el === 'object') && (el !== null);
+}
+
+/**
+ * @class BSON is a class for reading/writing files in BSON format, with NoSQL methods.
+ */
+
+class BSON {
+
+
+    //  row functions
+
+    /**
+     * @method constructor is used to instanciate a row of a collection saved in BSON.
+     * @param data number|object|null|undefined
+     */
+
+    constructor(data) {
+
+        this.sync_date = null;
+
+        if (is_object(data))
+            this.data = data;
+        else
+            this.data = {};
+    }
+
+
+    /**
+     * @method load is used to get informations associated with its id, then call ${next} as callback.
+     * @returns {BSON}
+     */
+
+    async load() {
+        let id = this.data.id,
+            self = this,
+            done = false,
+            file = this.getFile();
+        if (is_number(id)) {
+            await file.readEachLine((line) => {
+                if (!done) {
+                    let data = JSON.parse(line);
+                    if (data.id === id) {
+                        self.sync_date = Date.now();
+                        done = true;
+                        self.data = data;
+                    }
+                }
+            });
+        }
+        return this;
+    }
+
+
+    /**
+     * @method save is used to set informations, then call ${next} as a callback.
+     * @returns {BSON}
+     */
+
+    async save() {
+        let self = this.data;
+        if (this.data.id) {
+            await this.update((data) => {
+                if (data.id === self.id) {
+                    self.sync_date = Date.now();
+                    return self;
+                }
+            });
+        }
+        else {
+            await this.insert(self);
+            this.sync_date = Date.now();
+        }
+        return this;
+    }
+
+    /**
+     * @method get is used to get current row field value, or send undefined if not found.
+     * @param name
+     * @returns {*}
+     */
+
+    get(name) {
+        return this.data[name];
+    }
+
+    /**
+     * @method set is used to set row field value.
+     * @param name
+     * @param value
+     * @returns {BSON}
+     */
+
+    set(name, value) {
+        this.data[name] = value;
+        return this;
+    }
+
+
+    //  collection functions
+
+    /**
+     * @method select is used to seek each rows, and then use next as a callback.
+     * @param each function( Error err, Object data, Function then )
+     * @returns BSON
+     */
+
+    static async select(each) {
+
+        if (is_function(each)) {
+            let file = this.getFile();
+            await file.readEachLine(async function (line) {
+                await each(JSON.parse(line));
+            });
+        }
+        return this;
+    }
+
+
+    /**
+     * @method update is used to seek each rows and set them in third parameter of ${each} function
+     * @param each function( Error err, Object data, Function push )
+     * @return BSON
+     */
+
+    static async update(each) {
+        if (is_function(each)) {
+            let file = this.getFile();
+            await file.replaceEachLine(async function (line) {
+                let data = await each(JSON.parse(line));
+                if (data === BSON.UPDATE_REMOVE)
+                    return undefined;
+                if (data === BSON.UPDATE_IGNORE)
+                    return line;
+                if (data instanceof BSON)
+                    data = data.data;
+                return JSON.stringify(data);
+            });
+        }
+        return this;
+    }
+
+
+    /**
+     * @method insert is used to append a row with ${data}, then call ${next} as a callback.
+     * @param data Object
+     * @returns BSON
+     */
+
+    static async insert(data) {
+        if (is_object(data)) {
+            if (data instanceof BSON) data = data.data;
+            let file = this.getFile();
+            data.id = await this.nextId();
+            await file.append(JSON.stringify(data) + '\n');
+        }
+        return this;
+    }
+
+
+    /**
+     * @method nextId is used to get next unique id in these rows.
+     * @return {number}
+     */
+
+    static async nextId() {
+        let file = this.getFile();
+        let max = 1;
+        await file.readEachLine(function(line){
+            let data = JSON.parse(line);
+            if (data.id >= max)
+                max = data.id + 1;
+        });
+        return max;
+    }
+
+    static getFile() {
+        return File.build(BSON.FOLDER + this.name.toLowerCase() + '.bson');
+    }
+}
+
+BSON.FOLDER = 'C:/www/motor-js/data/';
+BSON.UPDATE_REMOVE = undefined;
+BSON.UPDATE_IGNORE = null;
+
+module.exports = BSON;
---
+++ file.js
@@ -0,0 +1,267 @@
+const pt = require('path');
+const fs = require('fs');
+
+function is_function(el) {
+    return (typeof el === 'function');
+}
+
+function is_string(el) {
+    return (typeof el === 'string');
+}
+
+class File {
+
+    constructor(path, chunk_size) {
+
+        path = pt.normalize(path);
+
+        Object.defineProperty(this, 'path', {'writable': false, 'value': path});
+        this.chunk_size = chunk_size || File.CHUNK_SIZE;
+
+        File.all[path] = this;
+    }
+
+
+    /**
+     * @function build is a static method called to be sure to have only one instance for this file.
+     * @param path
+     * @returns File
+     */
+
+    static build(path) {
+        path = pt.normalize(path);
+        let file = File.all[path];
+        if (file instanceof File)
+            return file;
+        else
+            return new File(path);
+    }
+
+
+    //  COMMON FILE
+
+    /**
+     * @function read is used to read entire file.
+     * @returns string
+     */
+
+    async read() {
+        return await fs.readFileSync(this.path, {'encoding':'utf8', 'flag':'r'});
+    }
+
+
+    /**
+     * @function write is used to write an entire file, create it or replace it.
+     * @param text {string}
+     * @returns {File}
+     */
+
+    async write(text) {
+        await fs.writeFileSync(this.path, text);
+        return this;
+    }
+
+
+    /**
+     * @function append is used to write at the end of a file.
+     * @param text {string}
+     * @returns {File}
+     */
+
+    async append(text) {
+        await fs.writeFileSync(this.path, text, {'encoding':'utf8', 'flag': 'a'});
+        return this;
+    }
+
+
+    /**
+     * @function exists is used to check if this file exists in this system.
+     * @returns {boolean}
+     */
+
+    async exists() {
+        try {
+            await fs.accessSync(this.path);
+            return true;
+        }
+        catch (e) {
+            return false;
+        }
+    }
+
+
+    /**
+     * @function size returns the file length in bytes.
+     * @returns {number}
+     */
+
+    async size() {
+        let fd = await fs.openSync(this.path, 'r'),
+            stat = await fs.fstatSync(fd);
+        await fs.closeSync(fd);
+        return stat.size;
+    }
+
+
+    /**
+     * @function rename move current file and returns its interface.
+     * @param newPath
+     * @returns {File}
+     */
+
+    async rename(newPath) {
+        await fs.rename(this.path, newPath);
+        return File.build(newPath);
+    }
+
+
+    /**
+     * @function unlink delete this file
+     * @throws {Error}
+     */
+
+    async unlink() {
+        await fs.unlink(this.path);
+    }
+
+
+    //  LARGE FILE
+
+    /**
+     * @function readEachChunk
+     * @param each {function({Buffer})}
+     * @param chunk_size {number}
+     */
+
+    async readEachChunk(each, chunk_size) {
+        let fd;
+        if (is_function(each) && (fd = await fs.openSync(this.path, 'r'))) {
+            let stat = await fs.fstatSync(fd),
+                bufferSize = stat.size;
+
+            if (bufferSize <= 1)
+                return null;
+
+            chunk_size = chunk_size || this.chunk_size;
+            let buffer = new Buffer(chunk_size),
+                bytesRead = 0;
+
+            let fn = async function () {
+                if (bytesRead < bufferSize) {
+                    let tmp_size;
+                    if ((bytesRead + chunk_size) >= bufferSize) {
+                        chunk_size = (bufferSize - bytesRead);
+                        tmp_size = await fs.readSync(fd, buffer, 0, chunk_size, bytesRead);
+                        bytesRead += tmp_size;
+                        await each(buffer.slice(0, tmp_size));
+                        await fs.closeSync(fd);
+                    }
+                    else {
+                        tmp_size = await fs.readSync(fd, buffer, 0, chunk_size, bytesRead);
+                        bytesRead += tmp_size;
+                        await each(buffer);
+                        await fn();
+                    }
+                }
+            };
+            await fn();
+            return false;
+        }
+        return null;
+    }
+
+
+    /**
+     * @function readEachLine is used to call function ${each) at each end of line.
+     * @param each {function({string})}
+     * @returns {number} of lines
+     */
+
+    async readEachLine(each) {
+        if (is_function(each)) {
+            let text = '',
+                line = '',
+                count = 0;
+
+            await this.readEachChunk(async function (buffer) {
+                text += buffer.toString();
+                let r;
+                while ((r = text.indexOf('\n')) !== -1) {
+                    line = text.substr(0, r).trim();
+                    text = text.substr(r + 1);
+                    await each(line);
+                    count++;
+                }
+            });
+
+            if (text.length) {
+                line = text.trim();
+                await each(line);
+                count++;
+            }
+            return count;
+        }
+        return null;
+    }
+
+
+    /**
+     * @function writeEachLine is used to write in a file each of these lines pushed in third parameter or ${each}
+     * @param each {function()}
+     * @returns {number} of lines inserted
+     */
+
+    async writeEachLine(each) {
+        if (is_function(each)) {
+            let fd = await fs.openSync(this.path, 'w'),
+                count = 0;
+
+            let fn = async function () {
+                let data = await each();
+                count++;
+                if (is_string(data)) {
+                    await fs.writeSync(fd, data + '\n');
+                    await fn();
+                }
+            };
+            await fn();
+            return count;
+        }
+    }
+
+
+    /**
+     * @function replaceEachLine is used to replace each line read in the file.
+     * @warn For performance settings, this function create a temp file (this.path + '_temp'). Beware there is no existing
+     * file with this path.
+     * @param each {function({string})}
+     * @returns {number}
+     */
+
+    async replaceEachLine(each) {
+        if (is_function(each)) {
+            let path = this.path + '_temp',
+                temp = await this.rename(path),
+                cache = [];
+
+            await temp.readEachLine(function(line){
+                cache.push(line);
+            });
+
+            let count = cache.length;
+
+            await this.writeEachLine(async function(){
+                return await each(cache.shift());
+            });
+
+            await temp.unlink();
+            return count;
+        }
+        return null;
+    }
+}
+
+File.all = [];
+File.CHUNK_SIZE = 0x10000;
+
+module.exports = File;
